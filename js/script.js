6e1bcdd4843139db447c69603940ccc66bbc9854
AzikZ <73075788+AzikZ@users.noreply.github.com>
2021-02-21 23:22:55 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,2 +1,15 @@
-let hello = "Привет";
-alert (hello);
+let num = 0;
+let score;
+
+const question1 = {
+	question: 'вопрос'
+}
+const question2 = {
+	question: 'вопрос'
+}
+const question3 = {
+	question: 'вопрос'
+}
+const question4 = {
+	question: 'вопрос'
+}
