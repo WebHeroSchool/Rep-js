e6d9c07669736934f32939646915f4f35a963c46
AzikZ <73075788+AzikZ@users.noreply.github.com>
2021-03-03 23:50:29 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,24 @@
-let num = 0;
-let score;
+const quest1 = {question: 'Чем является Солнце?', correctAnswer: 'звезда'}
+const quest2 = {question: 'Сколько материков на Земле?', correctAnswer: 'шесть'}
+const quest3 = {question: 'Какая планета четвертая от солнца ?', correctAnswer: 'марс'}
+const quest4 = {question: 'Какая нота и продукт называются одинаково?', correctAnswer: 'соль'}
 
-let arr = [
-	{question1:'',correctAnswer:'a'},
-	{question2:'',correctAnswer:'b'},
-	{question3:'',correctAnswer:'c'},
-	{question4:'',correctAnswer:'d'},
-]
+let score = 0;
+let quest = [quest1, quest2, quest3, quest4];
+
+function showResults() {
+  quest.forEach((item) => {
+  	let a = prompt(item.question);
+  	a = a.toLowerCase();
+  	answer = item.correctAnswer.toLowerCase(); 
+	if (answer === a) {
+  		alert ('Верно!');
+		score++
+  	} else {
+    		alert('Ошибка!')
+    	}
+  })
+  alert("Вы отгадали " + score + " загадки!");
+}
+
+showResults();
